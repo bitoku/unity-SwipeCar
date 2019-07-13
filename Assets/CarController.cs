8e914d57adcb202e6ca814d13dd8920d34ea61db
Ayato Tokubi <tokubi.ayato@gmail.com>
2019-07-13 14:11:28 +0900

    initial commit

---
+++ Assets/CarController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.startPos = Input.mousePosition;
+            //this.speed = 0.2f;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            this.speed = swipeLength / 1000.0f;
+
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+    }
+}
